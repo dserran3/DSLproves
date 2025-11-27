9767bc27f3b4557daeeabebfa1dc631a0fd656b0
dserran3 <dserran3@xtec.cat>
2025-11-27 09:54:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ export default defineConfig({
   plugins: [react()],
 })
 
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
+ {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
