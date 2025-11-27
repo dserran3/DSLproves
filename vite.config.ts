e42ae4f59606c6cc10d696de41b54ae3e7c12670
dserran3 <dserran3@xtec.cat>
2025-11-27 09:51:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/DSLproves/',  // importante para GitHub Pages
+  plugins: [react()],
+})
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
